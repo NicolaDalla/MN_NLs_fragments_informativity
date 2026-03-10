00877c558967b76cb81e94888ebe7c8c8a48b75a
Nicola Dalla Valle <nicola.dalla.valle96@gmail.com>
2026-03-10 16:38:35 +0100

    frag_info.R

--- frag_info.R
+++ frag_info.R
@@ -9,7 +9,7 @@ library(qgam)
 #perm: the number of permutaiton test for Z-score calcualtion; min_presence and max_presence: number of minimum and maximum node having a spectral features;
 #Q and K: QGAM parameters (Q=quantile, K=k)
 
-frag_info <- function(matrix, msms, perm=100, thr, min_presence=10, max_presence=length(msms), Q=0.8, K=5){
+frag_info <- function(matrix, msms, perm=100, thr, min_presence=10, max_presence=length(msms), Q=0.5, K=5){
   
   #listing the most frequent fragments
   mz_list <- msms$mz
@@ -187,6 +187,7 @@ plot_mz <- function(rs, msms, matrix, thr, n){
     )
   }
 }
+
 
 
 
